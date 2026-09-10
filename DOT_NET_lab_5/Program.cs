1dca8f914efc4369038f5df311ddee74afb30c07
omdeepsinhjadeja126630-wq <omdeepsinh.jadeja126630@marwadiuniversity.ac.in>
2026-09-10 11:30:09 +0530

    Add files via upload

---
+++ DOT_NET_lab_5/Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
